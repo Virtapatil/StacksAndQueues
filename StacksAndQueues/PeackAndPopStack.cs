d6aedc142f664a636bb17f7914a2e347c32684c8
Virtapatil2000 <virtapatil01@gmail.com>
2023-05-22 23:39:31 +0530

    Adding Peaking and Poping Stack

---
+++ StacksAndQueues/PeackAndPopStack.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueues
+{
+    public class PeackAndPopStack<T>
+    {
+        private LinkedList<T> stack;
+
+        public PeackAndPopStack()
+        {
+            stack = new LinkedList<T>();
+        }
+
+        public void Push(T value)
+        {
+            stack.AddFirst(value);
+        }
+
+        public T Pop()
+        {
+            if (stack.Count == 0)
+                throw new InvalidOperationException("The stack is empty.");
+
+            T value = stack.First.Value;
+            stack.RemoveFirst();
+            return value;
+        }
+
+        public T Peek()
+        {
+            if (stack.Count == 0)
+                throw new InvalidOperationException("The stack is empty.");
+
+            return stack.First.Value;
+        }
+
+        public bool IsEmpty()
+        {
+            return stack.Count == 0;
+        }
+    }
+
+}
--- StacksAndQueues/Program.cs
+++ StacksAndQueues/Program.cs
@@ -4,13 +4,30 @@
     {
         static void Main(string[] args)
         {
-            CreateStack<int> stack = new CreateStack<int>();
+            /*CreateStack<int> stack = new CreateStack<int>();
 
             stack.Push(70);
             stack.Push(30);
             stack.Push(56);
 
             Console.WriteLine("Current stack:");
+            while (!stack.IsEmpty())
+            {
+                int value = stack.Pop();
+                Console.WriteLine(value);
+            }
+
+            Console.ReadLine();*/
+
+            PeackAndPopStack<int> stack = new PeackAndPopStack<int>();
+
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+
+            Console.WriteLine("Peeking at the top of the stack: " + stack.Peek());
+
+            Console.WriteLine("Popping elements from the stack:");
             while (!stack.IsEmpty())
             {
                 int value = stack.Pop();
