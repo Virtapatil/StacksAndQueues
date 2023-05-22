6a12f024b2f73f2f91cc81eed8858b163198aefc
Virtapatil2000 <virtapatil01@gmail.com>
2023-05-22 23:26:27 +0530

    Creating a stack

--- StacksAndQueues/Program.cs
+++ StacksAndQueues/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            CreateStack<int> stack = new CreateStack<int>();
+
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+
+            Console.WriteLine("Current stack:");
+            while (!stack.IsEmpty())
+            {
+                int value = stack.Pop();
+                Console.WriteLine(value);
+            }
+
+            Console.ReadLine();
         }
     }
 }
